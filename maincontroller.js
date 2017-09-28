62259a307d7b92fc70108fafd397fca09ed2aa16
Ashley Smith <ashleymarie4989@gmail.com>
2017-09-27 23:43:51 -0500

    add modal

--- maincontroller.js
+++ maincontroller.js
@@ -3,6 +3,7 @@ app.controller("MainController", function($scope){
   $scope.submittedSurvey = false;
   $scope.makeAppointment = false;
   $scope.preferences = {};
+  $scope.testDriveSelection = null;
   $scope.submitSurvey = function(){
     var userQuery = {
       segment: $("#segment").val(),
@@ -16,9 +17,19 @@ app.controller("MainController", function($scope){
     console.log("finding vehicles for you that match ", userQuery);
     //TODO: Make ajax call with this to get vehicles
   }
+  $scope.openModal = function(id){
+    debugger;
+    this.testDriveSelection = this.vehicles.filter(function(v){
+      if(v.id = id) return true;
+    })[0];
+    var desc = "Test drive our " + this.testDriveSelection.year + " " + this.testDriveSelection.make + " " + this.testDriveSelection.model;
+    $("#drive-selection").val(desc);
+    $("#appt-modal").modal("show");
+  }
 
   $scope.vehicles = [
     {
+      id: 1,
       make: "Honda",
       model: "Civic",
       year: 2015,
@@ -33,6 +44,7 @@ app.controller("MainController", function($scope){
       gas: true
     },
     {
+      id: 2,
       make: "Chevy",
       model: "SUV",
       year: 2014,
@@ -47,6 +59,7 @@ app.controller("MainController", function($scope){
       gas: false
     },
     {
+      id: 3,
       make: "Ford",
       model: "F150",
       year: 2016,
